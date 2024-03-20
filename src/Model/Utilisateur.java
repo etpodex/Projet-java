68ceb0eb078d0413fc217de3a8054bc212008cbc
EnjoyBacon7 <59032058+EnjoyBacon7@users.noreply.github.com>
2024-03-20 11:54:15 +0100

    Added user model class

---
+++ src/Model/Utilisateur.java
@@ -0,0 +1,47 @@
+package Model;
+
+import java.util.Date;
+
+public class Utilisateur {
+    private String nom;
+    private String prenom;
+    private String email;
+    private Date dateNaissance;
+    private int nvMembre;
+
+    private boolean estEmploye;
+
+    public Utilisateur(String nom, String prenom, String email, Date dateNaissance, int nvMembre, boolean estEmploye) {
+        this.nom = nom;
+        this.prenom = prenom;
+        this.email = email;
+        this.dateNaissance = dateNaissance;
+        this.nvMembre = nvMembre;
+        this.estEmploye = estEmploye;
+
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public Date getDateNaissance() {
+        return dateNaissance;
+    }
+
+    public int getNvMembre() {
+        return nvMembre;
+    }
+
+    public boolean getEstEmploye() {
+        return estEmploye;
+    }
+}
