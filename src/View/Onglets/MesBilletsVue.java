a2197665e8940f40408d114c25b6f08279cec7fa
Akylum <augustin.poisson1@gmail.com>
2024-03-26 09:14:06 +0100

    Refonte Scroll MesBillets vu

--- src/View/Onglets/MesBilletsVue.java
+++ src/View/Onglets/MesBilletsVue.java
@@ -4,45 +4,34 @@ import View.Onglets.MesBilletsComposant.*;
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
 import java.awt.*;
-import java.awt.event.MouseWheelListener;
-import java.awt.event.MouseWheelEvent;
 
 public class MesBilletsVue extends JPanel {
 
     public MesBilletsVue(int panneau_contenu_width, int frame_height) {
         setBackground(new Color(40, 25, 125));
-        setLayout(new BorderLayout());
         setBorder(new EmptyBorder(20, 20, 20, 20));
-        panneau_contenu_width = panneau_contenu_width*14/15;
+
+        // Ajustement de la largeur du panneau de contenu
+        panneau_contenu_width = panneau_contenu_width * 14 / 15;
+
         // Création des composants
         AffichageBillet billet = new AffichageBillet(panneau_contenu_width, frame_height);
 
         // Création d'un panel interne pour le contenu défilable
-        JPanel contentPanel = new JPanel(new BorderLayout());
-        contentPanel.add(billet, BorderLayout.NORTH);
+        JPanel contentPanel = new JPanel();
+        contentPanel.setLayout(new BoxLayout(contentPanel, BoxLayout.Y_AXIS));
+
+        // Ajout du composant billet au panel interne
+        contentPanel.add(billet);
 
         // Création d'un JScrollPane qui contient le contentPanel
         JScrollPane scrollPane = new JScrollPane(contentPanel);
-        scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_NEVER);
+        scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_NEVER); // Cacher la barre de défilement verticale
         scrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
         scrollPane.setBorder(null);
 
-        // Ajout d'un MouseWheelListener pour gérer le défilement vertical
-        contentPanel.addMouseWheelListener(new MouseWheelListener() {
-            public void mouseWheelMoved(MouseWheelEvent e) {
-                JViewport viewport = scrollPane.getViewport();
-                Point viewPosition = viewport.getViewPosition();
-                int speed = scrollPane.getVerticalScrollBar().getUnitIncrement();
-                viewPosition.y += e.getWheelRotation() * speed;
-                int max = contentPanel.getPreferredSize().height - viewport.getHeight();
-                int min = 0;
-                if (viewPosition.y < min) viewPosition.y = min;
-                if (viewPosition.y > max) viewPosition.y = max;
-                viewport.setViewPosition(viewPosition);
-            }
-        });
-
         // Ajout du JScrollPane au panneau
+        setLayout(new BorderLayout());
         add(scrollPane, BorderLayout.CENTER);
     }
 }
