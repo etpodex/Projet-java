2a79930bb11a4874a510606d1e87b0bb64b3c2a7
Akylum <augustin.poisson1@gmail.com>
2024-04-21 09:24:43 +0200

    Afficheage offre 100% done

--- src/View/Onglets/AccueilVueComposant/OffreVue.java
+++ src/View/Onglets/AccueilVueComposant/OffreVue.java
@@ -7,16 +7,22 @@ import java.awt.*;
 
 public class OffreVue extends JPanel {
 
-    private int nombre_de_panel_offre = 3; // Nombre de panneaux correspondant au nombre d'offres
-    private double[] poids_panels = {0.33, 0.34, 0.33}; // Poids des panneaux ajusté pour trois offres
     private Offre[] offres = new Offre[]{
             new Offre("Offre1", 10, "CODE1"),
             new Offre("Offre2", 20, "CODE2"),
             new Offre("Offre3", 15, "CODE3")
     };
+    private int nombre_de_panel_offre = offres.length; // Nombre de panneaux correspondant au nombre d'offres
+    private double[] poids_panels; // Poids des panneaux pour le layout
 
     // Constructeur
     public OffreVue(int barre_navigation_panel_width, int frame_height) {
+        // Initialiser les poids des panneaux en fonction du nombre d'offres
+        poids_panels = new double[nombre_de_panel_offre];
+        for (int i = 0; i < nombre_de_panel_offre; i++) {
+            poids_panels[i] = 1.0 / nombre_de_panel_offre;
+        }
+
         // Calcul de la hauteur des panneaux
         int hauteur = frame_height * 1 / 3 - 30;
 
@@ -45,7 +51,7 @@ public class OffreVue extends JPanel {
             panel_offre.setBackground(new Color(255, 255 - 20 * i, 255 - 40 * i)); // Couleur de fond
 
             // Création du JLabel pour l'offre
-            String texteOffre = String.format("Avec le code %s obtenez %d%% de réduction", offres[i].getCode_promo(), offres[i].getReduction());
+            String texteOffre = String.format("<html><center>Avec le code %s <br>obtenez %d%% de réduction !</center></html>", offres[i].getCode_promo(), offres[i].getReduction());
             JLabel labelOffre = new JLabel(texteOffre, SwingConstants.CENTER);
             panel_offre.add(labelOffre, BorderLayout.CENTER); // Ajout du label au centre du panel
 
