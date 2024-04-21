345b3e9e81160bf0bd5dd748283c92ed55dbc439
EstelleHervault <163877255+EstelleHervault@users.noreply.github.com>
2024-04-21 19:26:00 +0200

    ConnexionVue javadoc ok

--- src/View/Onglets/ConnexionVue.java
+++ src/View/Onglets/ConnexionVue.java
@@ -3,11 +3,21 @@ package View.Onglets;
 import javax.swing.*;
 import java.awt.*;
 
+/**
+ * La classe ConnexionVue représente la vue de la page de connexion dans l'interface utilisateur.
+ */
 public class ConnexionVue extends JPanel {
+
+    /**
+     * Constructeur de la vue de la page de connexion.
+     *
+     * @param barre_navigation_panel_width La largeur de la barre de navigation.
+     * @param frame_height                La hauteur du frame.
+     */
     public ConnexionVue(int barre_navigation_panel_width, int frame_height){
 
-        setBackground(new Color(238, 238, 238));
-        setPreferredSize(new Dimension(barre_navigation_panel_width,frame_height));
+        setBackground(new Color(238, 238, 238));  // Définit la couleur de fond
+        setPreferredSize(new Dimension(barre_navigation_panel_width, frame_height));  // Définit la taille préférée
 
         // Revalide la mise en page + Redessine le panneau
         revalidate();
