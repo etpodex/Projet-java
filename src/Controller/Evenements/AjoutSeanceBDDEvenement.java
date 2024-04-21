1da841c2f62d7df450ee110160fdf6d87e6a44d6
EstelleHervault <163877255+EstelleHervault@users.noreply.github.com>
2024-04-21 20:22:20 +0200

    AjoutSeanceBDDEvenement

--- src/Controller/Evenements/AjoutSeanceBDDEvenement.java
+++ src/Controller/Evenements/AjoutSeanceBDDEvenement.java
@@ -2,14 +2,27 @@ package Controller.Evenements;
 
 import Model.Sceance;
 
+/**
+ * Cet événement représente l'ajout d'une séance dans la base de données.
+ */
 public class AjoutSeanceBDDEvenement {
 
-    private Sceance seance;
+    private Sceance seance;  // Séance à ajouter dans la base de données
 
+    /**
+     * Définit la séance à ajouter dans la base de données.
+     *
+     * @param seance La séance à ajouter
+     */
     public void setSeance(Sceance seance) {
         this.seance = seance;
     }
 
+    /**
+     * Obtient la séance à ajouter dans la base de données.
+     *
+     * @return La séance à ajouter
+     */
     public Sceance getSeance() {
         return seance;
     }
