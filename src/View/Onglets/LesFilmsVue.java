634df9d3e275730a0493a7e8338daf93f51f78b7
EstelleHervault <163877255+EstelleHervault@users.noreply.github.com>
2024-04-21 19:32:31 +0200

    LesFilmsVue javadoc ok

--- src/View/Onglets/LesFilmsVue.java
+++ src/View/Onglets/LesFilmsVue.java
@@ -1,5 +1,4 @@
 package View.Onglets;
-
 import Model.Film;
 import View.Onglets.LesFilmsComposant.Films;
 
@@ -7,34 +6,50 @@ import javax.swing.*;
 import javax.swing.border.EmptyBorder;
 import java.awt.*;
 
+/**
+ * Vue pour afficher la liste des films.
+ */
 public class LesFilmsVue extends JPanel {
 
     // Attributs
+
+    /** Tableau des films à afficher. */
     private Film[] films;
+
+    /** Nombre de films à afficher. */
     private int nombre_de_film;
 
+    /** Panneau pour afficher la liste des films. */
     private JPanel film_panel_liste;
+
+    /** ScrollPane pour permettre le défilement de la liste des films. */
     private JScrollPane scrollPane;
+
+    /** Panel pour envelopper le ScrollPane. */
     private JPanel wrapperPanel;
 
+    /** Largeur de la barre de navigation. */
     private int barre_navigation_panel_width;
 
-    // Constructeur
+    /**
+     * Constructeur de la vue pour afficher la liste des films.
+     *
+     * @param barre_navigation_panel_width Largeur de la barre de navigation.
+     * @param frame_height                Hauteur de la fenêtre principale.
+     */
     public LesFilmsVue(int barre_navigation_panel_width, int frame_height) {
-
         setBackground(new Color(238, 238, 238)); // Définir la couleur de fond
         setBorder(new EmptyBorder(20, 80, 20, 80)); // Définir les marges intérieures
         setPreferredSize(new Dimension(barre_navigation_panel_width, frame_height)); // Définir la taille préférée
 
-        BorderLayout layout = new BorderLayout();
-        setLayout(layout); // Utiliser un BorderLayout pour organiser les composants
+        setLayout(new BorderLayout()); // Utiliser un BorderLayout pour organiser les composants
 
-        this.film_panel_liste = new JPanel();
-        this.film_panel_liste.setLayout(new BoxLayout(this.film_panel_liste, BoxLayout.Y_AXIS)); // Utiliser un BoxLayout vertical pour organiser les films
+        film_panel_liste = new JPanel();
+        film_panel_liste.setLayout(new BoxLayout(film_panel_liste, BoxLayout.Y_AXIS)); // Utiliser un BoxLayout vertical pour organiser les films
 
         this.barre_navigation_panel_width = barre_navigation_panel_width;
 
-        this.scrollPane = new JScrollPane();
+        scrollPane = new JScrollPane();
         scrollPane.setViewportView(film_panel_liste);
         scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_NEVER); // Cacher la barre de défilement verticale
         scrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER); // Cacher la barre de défilement horizontale
@@ -45,10 +60,14 @@ public class LesFilmsVue extends JPanel {
         add(wrapperPanel, BorderLayout.CENTER); // Ajouter le JScrollPane au centre du panneau
     }
 
-    // Méthode pour mettre à jour les films affichés
+    /**
+     * Met à jour les films affichés.
+     *
+     * @param films Tableau des films à afficher.
+     */
     public void updateFilms(Film[] films) {
         this.films = films;
-        this.nombre_de_film = films.length;
+        nombre_de_film = films.length;
 
         film_panel_liste.removeAll(); // Supprimer tous les films précédemment affichés
 
