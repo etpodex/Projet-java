8b7704acf0716ef9e48e06dbe8e59a1fbd2c0aff
EstelleHervault <163877255+EstelleHervault@users.noreply.github.com>
2024-04-21 20:22:48 +0200

    AjoutFilmBDDEvenement

--- src/Controller/Evenements/AjoutFilmBDDEvenement.java
+++ src/Controller/Evenements/AjoutFilmBDDEvenement.java
@@ -2,17 +2,27 @@ package Controller.Evenements;
 
 import Model.Film;
 
+/**
+ * Cet événement représente l'ajout d'un film dans la base de données.
+ */
 public class AjoutFilmBDDEvenement {
 
-    Film film;
+    private Film film;  // Film à ajouter dans la base de données
 
-    public AjoutFilmBDDEvenement() {
-
-    }
+    /**
+     * Définit le film à ajouter dans la base de données.
+     *
+     * @param film Le film à ajouter
+     */
     public void setFilm(Film film) {
         this.film = film;
     }
 
+    /**
+     * Obtient le film à ajouter dans la base de données.
+     *
+     * @return Le film à ajouter
+     */
     public Film getFilm() {
         return film;
     }
