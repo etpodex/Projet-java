ce8b63d1d256df79ab9328e3ff89a4c91b71b439
EstelleHervault <163877255+EstelleHervault@users.noreply.github.com>
2024-04-21 19:23:34 +0200

    AccueilVue javadoc ok

--- src/View/Onglets/AccueilVue.java
+++ src/View/Onglets/AccueilVue.java
@@ -8,11 +8,20 @@ import javax.swing.*;
 import javax.swing.border.EmptyBorder;
 import java.awt.*;
 
+/**
+ * La classe AccueilVue représente la vue principale pour l'onglet Accueil.
+ * Elle contient une vue d'offre en haut et une vue de calendrier en bas.
+ */
 public class AccueilVue extends JPanel {
 
     private Calendrier calendrier;  // Référence à la vue du calendrier
 
-    // Constructeur prenant la largeur de la barre de navigation et la hauteur du frame comme paramètres
+    /**
+     * Constructeur prenant la largeur de la barre de navigation et la hauteur du frame comme paramètres.
+     *
+     * @param barre_navigation_panel_width La largeur de la barre de navigation.
+     * @param frame_height                  La hauteur du frame.
+     */
     public AccueilVue(int barre_navigation_panel_width, int frame_height){
         setBackground(new Color(238, 238, 238));  // Définir la couleur de fond
         setPreferredSize(new Dimension(barre_navigation_panel_width,frame_height));  // Définir la taille préférée du panneau
@@ -29,10 +38,20 @@ public class AccueilVue extends JPanel {
         add(calendrier, BorderLayout.SOUTH);  // Ajouter Calendrier au sud du panneau
     }
 
+    /**
+     * Met à jour le graphique circulaire dans la vue du calendrier avec le jeu de données spécifié.
+     *
+     * @param dataset Le jeu de données pour le graphique circulaire.
+     */
     public void setPieChart(DefaultPieDataset dataset) {
         this.calendrier.setPieChart(dataset);  // Appeler la méthode setPieChart de Calendrier
     }
 
+    /**
+     * Met à jour le graphique à barres dans la vue du calendrier avec le jeu de données spécifié.
+     *
+     * @param dataset Le jeu de données pour le graphique à barres.
+     */
     public void setBarChart(DefaultCategoryDataset dataset) {
         this.calendrier.setBarChart(dataset);  // Appeler la méthode setBarChart de Calendrier
     }
