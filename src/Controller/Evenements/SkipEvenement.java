8f5c22e9ebc0ad502978af4a5931108baab2227f
EstelleHervault <163877255+EstelleHervault@users.noreply.github.com>
2024-04-21 20:17:07 +0200

    SkipEvenement javadoc ok

--- src/Controller/Evenements/SkipEvenement.java
+++ src/Controller/Evenements/SkipEvenement.java
@@ -1,5 +1,13 @@
 package Controller.Evenements;
 
+/**
+ * Événement permettant de passer à l'étape suivante ou de passer outre une étape dans le processus.
+ */
 public class SkipEvenement {
 
+    /**
+     * Constructeur par défaut de l'événement de saut.
+     */
+    public SkipEvenement() {
+    }
 }
